e5d99284cbef44a371d34734f2539ebab1616927
EXlena7-7 <marjor071101@gmail.com>
2024-05-24 15:37:39 -0400

    app

---
+++ src/GifExpertApp.js
@@ -0,0 +1,31 @@
+
+import React, { useState } from 'react';
+import { AddCategory } from './components/AddCategory';
+import { GifGrid } from './components/GifGrid';
+
+export const GifExpertApp = () => {
+   const [categories, setCategories] = useState(['Miku']);
+
+   console.log(categories);
+  return (
+    <>
+        <h2>Tienda de Gifs</h2>
+        <AddCategory setCategories={ setCategories }/>
+        
+            <hr />
+       
+        <ol>
+            {
+                categories.map(category =>
+                    <GifGrid 
+                    key={category}
+                    category={category}
+                    />
+                    // <li key={category}>{category}</li>
+                )
+            }
+        </ol>
+       
+    </>
+  )
+}
